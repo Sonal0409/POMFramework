ab14bdc1676ae927e388402167a212d3209a3ecb
Sonal <mittal.sonal04@gmail.com>
2020-03-13 06:14:33 -0400

    added new testcase java files

---
+++ src/main/java/com/qa/testdata/Testcase1.java
@@ -0,0 +1,12 @@
+package com.qa.testdata;
+
+public class Testcase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("created new test case");
+
+	}
+
+}
---
+++ src/main/java/com/qa/testdata/Testcase2.java
@@ -0,0 +1,10 @@
+package com.qa.testdata;
+
+public class Testcase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("created new test case2");
+	}
+
+}
